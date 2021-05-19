e40f1c7ddababe6af96444ff2f1ec82cb231a4fb
LZHangSlow <2456009787@qq.com>
2021-05-19 19:33:25 +0800

    thymeleaf的练习

---
+++ src/main/java/com/example/admin/bean/User.java
@@ -0,0 +1,14 @@
+package com.example.admin.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private String username;
+    private String password;
+}
